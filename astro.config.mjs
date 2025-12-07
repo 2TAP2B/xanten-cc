dcd8eccd7d1e04a53239bd59fae25a0bfa0b8c91
Tobias <steltner@pm.me>
2025-12-08 00:17:40 +0100

    Use passthrough image service to avoid Sharp dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
+import { passthroughImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
+  image: {
+    service: passthroughImageService(),
+  },
   server: {
     port: 3000,
     host: true,
