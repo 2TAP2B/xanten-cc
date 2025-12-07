89d56ebb4a6304cdff9121a5251234a70d286c7c
Tobias <steltner@pm.me>
2025-12-07 20:42:28 +0100

    claude

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,18 @@
 import { defineConfig } from "astro/config";
-import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app/",
+  site: "https://example.com",
   server: {
     port: 3000,
     host: true,
-  },
-  vite: {
-    server: {
-      allowedHosts: ["xanten.build.dr0p.space"],
-    },
   },
   integrations: [
     tailwind(),
     icon(),
     sitemap(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
   ],
 });
