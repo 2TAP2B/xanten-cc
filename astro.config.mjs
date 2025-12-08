525844d343fe0daf78679638ea25322fb42ac34e
Tobias <steltner@pm.me>
2025-12-08 12:31:53 +0100

    Use noop image service instead of passthrough for proper static builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
-import { passthroughImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   image: {
-    service: passthroughImageService(),
+    service: {
+      entrypoint: "astro/assets/services/noop"
+    },
   },
   server: {
     port: 3000,
